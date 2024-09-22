438e0722ad8356c214149a2bdf95d1997461a122
mohd-minhal <mohdminhal.001@gmail.com>
2024-09-22 21:47:57 +0530

    all test cases passed

--- tests/students_test.py
+++ tests/students_test.py
@@ -58,21 +58,21 @@ def test_post_assignment_student_1(client, h_student_1):
     assert data['teacher_id'] is None
 
 
-def test_submit_assignment_student_1(client, h_student_1):
+def test_submit_assignment_student_2(client, h_student_2):
     response = client.post(
         '/student/assignments/submit',
-        headers=h_student_1,
+        headers=h_student_2,
         json={
-            'id': 2,
-            'teacher_id': 2,
+            'id': 3,
+            'teacher_id': 1,
         })
 
     assert response.status_code == 200
 
     data = response.json['data']
-    assert data['student_id'] == 1
-    assert data['state'] == 'SUBMITTED'
-    assert data['teacher_id'] == 2
+    assert data['student_id'] == 2
+    assert data['state'] == 'DRAFT'
+    assert data['teacher_id'] == 1
 
 
 def test_assignment_resubmit_error(client, h_student_1):
