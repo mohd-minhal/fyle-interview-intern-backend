469ef789cb85ca203bc5c3f69cdc9dc5a8058a20
mohd-minhal <mohdminhal.001@gmail.com>
2024-09-22 21:13:48 +0530

    written sql queries for test cases

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,5 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+SELECT COUNT(*) 
+FROM assignments 
+WHERE teacher_id = :teacher_id 
+AND grade = 'A';
